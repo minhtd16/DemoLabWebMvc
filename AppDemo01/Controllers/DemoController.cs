cc5e6a41888a7a8a8844f95f42c671f5861a2fff
minhtd16 <minhtd16@gmail.com>
2024-01-19 22:48:36 +0700

    init
    #init

---
+++ AppDemo01/Controllers/DemoController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppDemo01.Controllers
+{
+    public class DemoController : Controller
+    {
+        public string Vidu01()
+        {
+            return "Học lập trình web MVC";
+        }
+        public int Vidu02()
+        {
+            int a = 20, b = 30;
+            int Tong = a + b;
+            return Tong;
+        }
+        public ActionResult Vidu03()
+        {
+            string str = "Học lập trình web MVC";
+            ViewBag.str = str;
+            return View();
+        }
+        public ActionResult Vidu04()
+        {
+            string str = "Học lập trình web MVC";
+            ViewData["str"] = str;
+            return View();            
+        }
+
+        public ActionResult Vidu05()
+        {
+            string tieuDe = "Cách viết mã nguồn C# trên tệp .cshtml";
+            ViewBag.tieuDe = tieuDe;
+            return View();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ AppDemo02/Controllers/DemoController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppDemo02.Controllers
+{
+    public class DemoController : Controller
+    {       
+        public ActionResult Vidu01()
+        { 
+            /*Chú thích trong C# */
+            ViewBag.Title = "Các điều khiển cơ bản trong .cshtml";
+            return View();
+        }
+        public ActionResult Vidu02()
+        {
+            /*Chú thích trong C# */
+            ViewBag.Title = "Cách nhúng mã nguồn C# trên cshtml";
+            ViewBag.Content = "Dùng vòng lặp for để tạo table";
+            return View();
+        }
+        public ActionResult Vidu03()
+        {
+            /*Chú thích trong C# */
+            ViewBag.Title = "Cách nhúng mã nguồn C# trên cshtml";
+            ViewBag.Content = "Dùng vòng lặp for để tạo table";
+            return View();
+        }
+        public ActionResult Vidu04()
+        {
+            /*Chú thích trong C# */
+            ViewBag.Title = "Cách nhúng mã nguồn C# trên cshtml";
+            ViewBag.Content = "Dùng vòng lặp for để tạo table";
+            return View();
+        }
+        public ActionResult Vidu05()
+        {
+            string[] ListHoTen = { "Lê Hoàng", "Trịnh Tráng", "Quang Cảnh", "Hồng Quân" };
+            ViewData["ListHoTen"] = ListHoTen;
+            return View();            
+        }        
+        public ActionResult Vidu06()
+        {
+            /*Chuyển trang trong MVC */
+            ViewBag.Title = "Chuyển trang trong MVC";             
+            return View();
+        }
+        public ActionResult ChuyenTrang()
+        {                 
+            return RedirectToAction("Vidu05");
+        }
+        // truyền dữ liệu từ View đến controller:
+        public ActionResult Login()
+        {            
+            return View();
+        }
+        [HttpPost, ActionName("Login")]
+        public ActionResult DangNhapHeThong(string TenDangNhap, string MatKhau)
+        {          
+            if(TenDangNhap.ToUpper() =="DHCNTTK25A" && MatKhau.ToUpper() == "DHCNTTK25A")
+            {
+                return RedirectToAction("Vidu05");
+            }
+            ViewBag.Ten = TenDangNhap;
+            ViewBag.Pas = MatKhau;
+            ViewBag.Erorr = "Vui lòng nhập đúng tên đăng nhập và mật khẩu";
+            return View();
+        }
+    }
+}
+
+
+
+
+
